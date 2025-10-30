2736eee27e696adb3da4f7f3fc31282e9d802596
POWERTUBE911 <bonsmmm@gmail.com>
2025-10-30 10:21:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-FIREBASE_URL = "https://gang-war-2-default-rtdb.europe-west1.firebasedatabase.app"
+FIREBASE_URL = "https://gang-war-2-default-rtdb.europe-west1.firebasedatabase.app/"
 FIREBASE_SECRET = None
